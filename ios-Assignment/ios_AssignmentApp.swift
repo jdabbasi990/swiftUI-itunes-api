03bb7fa9be3c55c09c63f83977745a47a2832006
Jawwad Abbasi <jawwadabbasi990@gmail.com>
2024-02-19 19:22:05 -0500

    Initial Commit

---
+++ ios-Assignment/ios_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_AssignmentApp.swift
+//  ios-Assignment
+//
+//  Created by Jawwad Abbasi on 2024-02-19.
+//
+
+import SwiftUI
+
+@main
+struct ios_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
