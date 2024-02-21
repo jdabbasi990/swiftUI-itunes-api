7f1aad31128d38e8cda8649339fb373e31e93a34
Jawwad Abbasi <jawwadabbasi990@gmail.com>
2024-02-21 00:15:35 -0500

    Image and album data displayed.

--- ios-Assignment/ContentView.swift
+++ ios-Assignment/ContentView.swift
@@ -6,9 +6,40 @@
 //
 
 import SwiftUI
+@propertyWrapper
+struct IgnoreFailure<Value:Decodable>: Decodable{
+    var wrappedValue: [Value] = []
+    private struct _None: Decodable{}
+    init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+        while !container.isAtEnd{
+            if let decoded = try?
+                container.decode(Value.self){
+                wrappedValue.append(decoded)
+            } else {
+                _ = try? container.decode(_None.self)
+            }
+        }
+    }
+}
 
+
+// struct to get responses from URL
+struct TrackResponse: Decodable {
+    var resultCount: Int
+    @IgnoreFailure
+    var results: [Track]
+}
+
+struct AlbumResponse: Decodable {
+    var resultCount: Int
+    @IgnoreFailure
+    var results: [Album]
+}
+
+// struct to get track and album
 struct Track: Codable{
-    var trackID: Int
+    var trackId: Int
     var trackName: String
     var collectionName: String
 }
@@ -19,16 +50,63 @@ struct Album: Codable {
     var collectionName: String
 }
 
+// Main content view
 struct ContentView: View {
+    @State private var tracks = [Track]()
+    @State private var album = [Album]()
+    @State private var albumArt = ""
+    @State private var albumTitle = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text(albumTitle)
+            .fontWeight(.bold)
+            .font(.system(size: 20))
+            .padding()
+        AsyncImage(url: URL(string: albumArt), content: {image in
+            image.resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+                .padding()
+        }, placeholder: {
+            Image(.greySquare)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+                .padding()
+        })
+        List (tracks, id:\.trackId){ item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
     }
+    
+    // function to load data from url
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/lookup?id=1710685602&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(TrackResponse.self, from: data) {
+                tracks = decodedResponse.results
+            }
+            if let decodedResponse = try? JSONDecoder().decode(AlbumResponse.self, from: data) {
+                album = decodedResponse.results
+                albumArt = (album[0].artworkUrl100).replacingOccurrences(of: "100x100bb.jpg", with: "800x800bb.jpg")
+                albumTitle = album[0].collectionName
+            }
+        } catch {
+            print("Invalid Data")
+        }
+    }
+    
 }
 
 #Preview {
